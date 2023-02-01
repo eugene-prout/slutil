174f2717e7466079965a0688079ed25ae1da782e
Eugene <eugene.prout1@gmail.com>
2023-02-01 16:30:53 +0000

    fix: move slurm calls into separate file

---
+++ slutil/slurm.py
@@ -0,0 +1,32 @@
+import subprocess
+import re
+
+def get_job_status(job_id: int):
+    if not test_slurm_accessible():
+        raise OSError("Slurm accessed required but cannot access Slurm")
+
+    regex_pattern = r"^(\s*JobID\s*JobName\s*Partition\s*Account\s*AllocCPUS\s*State\s*ExitCode\s*)(-*\s*){7}(\S*\s*)(\S*\s*)(\S*\s*)(\S*\s*)(\S*\s*)(\S*\s*)(\S*\s*){7}$"
+    output = subprocess.check_output(["sacct", "-j", str(job_id)]).strip().decode()
+    regex_match = re.match(regex_pattern, output)
+    if regex_match:
+        return regex_match.group(8).strip()
+    raise OSError("sacct command has unexpected output")
+
+def submit_job(sbatch: str) -> int:
+    if not test_slurm_accessible():
+            raise OSError("Slurm accessed required but cannot access Slurm")
+    
+    proc = subprocess.run(f'sbatch {sbatch}', check=True, capture_output=True, shell=True)        
+    # proc.stdout should be "Submitted batch job XXXXXX"
+    regex_match = re.match(r"^(Submitted batch job )(\d*)$", proc.stdout.decode("utf-8"))
+    if regex_match:
+        return int(regex_match.group(2))
+    raise OSError("sbatch command has unexpected output")
+
+
+def test_slurm_accessible():
+    try: 
+        subprocess.run(["sinfo"], capture_output=True, check=True)
+        return True
+    except:
+        return False
