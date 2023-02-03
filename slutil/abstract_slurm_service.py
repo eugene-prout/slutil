245d3410dc52da6427d8202dc1d72725e724a4b8
Eugene <eugene.prout1@gmail.com>
2023-02-03 15:26:10 +0000

    refactor: define slurm interface in abc

---
+++ slutil/abstract_slurm_service.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class AbstractSlurmService(ABC):
+    @staticmethod
+    @abstractmethod
+    def get_job_status(job_id: int) -> str:
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def submit_job(sbatch: str) -> int:
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def test_slurm_accessible() -> bool:
+        raise NotImplementedError
