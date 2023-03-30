94ced80a6a442538a3556d43667cd0744db26ac2
Eugene Prout <eugene.prout@gmail.com>
2023-03-30 20:29:26 +0100

    feat: add [d] to commit tag if dirty repo

--- slutil/adapters/git.py
+++ slutil/adapters/git.py
@@ -1,3 +1,4 @@
+import logging
 from slutil.adapters.abstract_vcs import AbstractVCS
 import subprocess
 
@@ -11,6 +12,19 @@ class Git(AbstractVCS):
                 .strip()
                 .decode()
             )
+
+            status = (
+                subprocess.check_output(["git", "status", "--porcelain"])
+                .decode()
+            )
+
+            # slutil writes to two files (.slutil_job_hisotry.csv and debug.log) 
+            # they show up as updated or untracked so we allow them to be changed
+            if status.count("\n") > 2:
+                logging.debug("git directory dirty, adding [d] to commit tag")
+                output += "[d]"
+
             return output
-        except subprocess.CalledProcessError:
+        except subprocess.CalledProcessError as e:
+            logging.debug("error running %s, stderr: %s, stdout: %s", e.cmd, e.stderr, e.stdout)
             return "UNKNOWN"
